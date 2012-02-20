3827764d72a6f1661ee7b16e4d726985c36a2972
Rawane ZOSSOU <dev@raw1z.fr>
2012-02-20 05:20:34 +0100

    made the install generator copy the migrations instead of creating new ones in the receiving app

--- lib/generators/cargo_wiki/install_generator.rb
+++ lib/generators/cargo_wiki/install_generator.rb
@@ -14,15 +14,11 @@ module CargoWiki
       def copy_migrations_files
         empty_directory "db/migrate"
 
-        migration_template "create_cargo_wiki_articles.rb", "db/migrate/create_cargo_wiki_articles.rb"
-        migration_template "create_cargo_wiki_users.rb", "db/migrate/create_cargo_wiki_users.rb"
-        migration_template "add_author_id_to_articles.rb", "db/migrate/add_author_id_to_articles.rb"
-        migration_template "add_auth_token_to_users.rb", "db/migrate/add_auth_token_to_users.rb"
-        migration_template "add_role_to_users.rb", "db/migrate/add_role_to_users.rb"
-        migration_template "create_versions.rb", "db/migrate/create_versions.rb"
-        migration_template "add_commit_message_to_versions.rb", "db/migrate/add_commit_message_to_versions.rb"
-        migration_template "acts_as_taggable_on_migration.rb", "db/migrate/acts_as_taggable_on_migration.rb"
-        migration_template "add_published_to_cargo_wiki_articles.rb", "db/migrate/add_published_to_cargo_wiki_articles.rb"
+        migrations_folder = File.expand_path("../../../../db/migrate", __FILE__)
+        Dir["#{migrations_folder}/*.rb"].each do |file|
+          filename = File.basename(file)
+          copy_file file, "db/migrate/#{filename}"
+        end
       end
     end
   end
